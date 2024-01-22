35104559a997a5e30354b323eaaeabfa2937100c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-01-21 22:07:27 -0600

    feat: Add function to write a list of files to a new file
    This function writes a file where every line is a new filename. This
    file will be compared against later.

--- src/main.rs
+++ src/main.rs
@@ -17,5 +17,18 @@ fn file_exists(filename: &str) -> bool {
     }
 }
 
+fn write_list_of_files_in_current_directory_to_file() {
+    let paths = fs::read_dir("./").unwrap();
+    let mut file = fs::File::create(".clean_home").unwrap();
+
+    for path in paths {
+        let path = path.unwrap().path();
+        let path = path.display().to_string();
+        file.write_all(path.as_bytes()).unwrap();
+        file.write_all(b"\n").unwrap();
+    }
+}
+
 fn main() {
+    write_list_of_files_in_current_directory_to_file();
 }
